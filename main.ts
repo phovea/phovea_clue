bd7d85ff238d34afec289c686053076d75ed9a03
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-08-27 17:40:24 +0200

    simple adding of elements from databrowser

--- main.ts
+++ main.ts
@@ -4,8 +4,9 @@
 /// <amd-dependency path='font-awesome' />
 /// <amd-dependency path='bootstrap' />
 
+import vis = require('../caleydo_core/vis');
+import datatypes = require('../caleydo_core/datatype');
 import prov = require('../caleydo_provenance/main');
-import prov_sel = require('../caleydo_provenance/selection');
 import d3 = require('d3');
 
 var a = 5;
@@ -17,17 +18,75 @@ var graph = prov.create({
   id: 'clue'
 });
 
+import prov_sel = require('../caleydo_provenance/selection');
 prov_sel.create(graph, 'selected', {
   filter: function (idtype) {
     return idtype && idtype.name[0] !== '_';
   }
 });
 
-import selection = require('../caleydo_core/selectioninfo');
-selection.create(document.querySelector('aside.left'));
-
 import databrowser = require('../caleydo_window/databrowser');
-databrowser.create(document.querySelector('aside.left'));
+databrowser.create(document.querySelector('#databrowser'));
+
+import selection = require('../caleydo_core/selectioninfo');
+selection.create(document.querySelector('#selectioninfo'));
+
+var $main = d3.select('main');
+var $main_ref =  graph.addObject($main, 'Board', 'visual');
+
+function addElem(inputs, parameter) {
+    var $main = inputs[0].v,
+      data = inputs[1].v,
+      pos = parameter.pos;
+  var $div = $main.append('div').classed('block', true).datum(data).style({
+    left: pos.x+'px',
+    top: pos.y+'px'
+  });
+  var $toolbar = $div.append('div').classed('toolbar', true);
+  $toolbar.append('i').attr('class', 'fa fa-close').on('click',() => {
+    graph.push(createRemoveCmd($div_ref));
+  });
+  var $body = $div.append('div').classed('body', true);
+  vis.list(data)[0].load().then((p) => {
+    p.factory(data, $body.node());
+  });
+  var $div_ref = prov.ref($div, 'Block '+data.desc.name, prov.cat.visual);
+
+  return {
+    created: [$div_ref],
+    inverse: createRemoveCmd($div_ref)
+  }
+}
+function removeElem(inputs) {
+  var $div = inputs[0].v,
+    inv = createAddCmd($main_ref, graph.findObject($div.datum()), {
+      x : parseInt($div.style('left')),
+      y : parseInt($div.style('top'))
+    });
+  $div.remove();
+  return {
+    removed: [inputs[0]],
+    inverse: inv
+  };
+}
+
+interface ID3Ref extends prov.IObjectRef<d3.Selection<any>> {
+
+}
+
+function createAddCmd($main_ref: ID3Ref, data: prov.IObjectRef<datatypes.IDataType>, pos: {x: number; y:number}) {
+  return prov.action(prov.meta('Block for '+data.v.desc.name, prov.cat.visual, prov.op.create), 'addElem', addElem, [$main_ref, data], {
+    pos: pos
+  });
+}
+function createRemoveCmd($div_ref: ID3Ref) {
+  return prov.action(prov.meta('Remove Block', prov.cat.visual, prov.op.remove), 'removeColumn', removeElem, [$div_ref]);
+}
+
+databrowser.makeDropable(<Element>$main.node(), (data, op, pos) => {
+  var data_ref = graph.findOrAddObject(data, data.desc.name, 'data');
+  graph.push(createAddCmd($main_ref, data_ref, pos));
+});
 
 export function dummy() {
   return a;
