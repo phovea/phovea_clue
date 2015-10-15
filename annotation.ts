cd0dda721cd207c6dcc230c0603710dacda7ed2b
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-10-15 17:22:17 +0200

    work on annotation editing

--- annotation.ts
+++ annotation.ts
@@ -21,12 +21,16 @@ export class Renderer {
 
   private prev = Promise.resolve(null);
 
+  private renderer;
+
   constructor(private $main:d3.Selection<any>, private graph:prov.ProvenanceGraph, options = {}) {
     C.mixin(this.options, options);
+
+    this.renderer = (d:string) => modeFeatures.isEditable() && d.length === 0 ? '<i>Enter Text by Clicking (MarkDown supported)</i>' : (this.options.markdown ? marked(d) : d);
   }
 
   render(state:prov.AStoryNode) {
-    //createa full chain
+    //create full chain
     this.prev = this.prev.then(() => {
       var takedown = this.hideOld();
       if (!state) {
@@ -37,38 +41,139 @@ export class Renderer {
         next = this.renderText(<prov.TextStoryNode>state);
       }
       if (state instanceof prov.JumpToStoryNode) {
-        next = Promise.all([this.graph.jumpTo(state.state), this.renderAnnotations(state.annotations)]);
+        next = this.graph.jumpTo(state.state);
       }
-      return Promise.all([takedown, next]);
+      return Promise.all([takedown, next, this.renderAnnotations(state)]);
     });
     return this.prev;
   }
 
-  renderAnnotations(anns:prov.IStateAnnotation[]) {
-    return new Promise((resolve) => {
-      const $anns = this.$main.selectAll('div.text-annotation').data(anns);
-      $anns.enter().append('div').classed('text-annotation', true).style('opacity', 0);
-      $anns.html((d) => this.options.markdown ? marked(d.text) : d.text);
-      $anns.style({
-        width: (d:prov.IStateAnnotation) => d.size ? d.size[0] + 'px' : null,
-        height: (d:prov.IStateAnnotation) => d.size ? d.size[1] + 'px' : null,
-        transform: (d:prov.IStateAnnotation) => `translate(${d.pos[0]}px,${d.pos[1]}px)rotate(${d.rotation || 0}deg)scale(${d.scale ? d.scale[1] : 1},${d.scale ? d.scale[1] : 1})`
+  private renderAnnotationsImpl(state:prov.AStoryNode) {
+    const that = this;
+    const editable = modeFeatures.isEditable();
+
+
+    const $anns = this.$main.selectAll('div.text-annotation').data(state.annotations);
+    const $anns_enter = $anns.enter().append('div').classed('text-annotation', true);
+    let onEdit = function (d:prov.IStateAnnotation, i) {
+      const $elem = d3.select(this);
+      if (!d3.select(this.parentNode).classed('editable')) {
+        return;
+      }
+      $elem.on('click', null);
+      $elem.append('textarea').property('value', d.text).on('blur', function () {
+        d.text = this.value;
+        state.setAnnotation(i, d);
+        //update value and enable edit click handler again
+        $elem.html(that.renderer(this.value)).on('click', onEdit);
       });
+    };
+
+    $anns_enter.append('div').on('click', onEdit);
+
+    $anns_enter.classed('editable',editable);
+    const updateTransform = (d:prov.IStateAnnotation) => `rotate(${d.rotation || 0}deg)scale(${d.scale ? d.scale[1] : 1},${d.scale ? d.scale[1] : 1})`;
+
+    if (editable) {
+      //remove
+      $anns_enter.append('button').attr('tabindex',-1).attr('class', 'btn btn-default fa fa-remove').on('click', function (d:prov.IStateAnnotation, i) {
+        d3.select(this.parentNode).remove();
+        state.removeAnnotation(i);
+      });
+      //move
+      $anns_enter.append('button').attr('tabindex',-1).attr('class', 'btn btn-default fa fa-arrows').call(d3.behavior.drag()
+        .origin((d:prov.IStateAnnotation) => ({x: d.pos[0], y: d.pos[1]}))
+        .on('drag', function (d:prov.IStateAnnotation, i) {
+          const e:any = d3.event;
+          console.log(e.dx, e.dy, e.x, e.y);
+          d.pos = [e.x, e.y];
+          state.setAnnotation(i, d);
+          d3.select(this.parentNode).style('left', d.pos[0]+'px').style('top', d.pos[1]+'px');
+        }));
+      //resize
+      $anns_enter.append('button').attr('tabindex',-1).attr('class', 'btn btn-default fa fa-expand fa-flip-horizontal')
+        .on('dblclick', function(d:prov.IStateAnnotation,i) {
+          //remove the fixed size
+          delete d.size;
+          state.setAnnotation(i, d);
+          d3.select(this.parentNode).style({
+            width: null,
+            height: null
+          });
+        })
+        .call(d3.behavior.drag()
+          .origin((d:prov.IStateAnnotation) => ({x: d.pos[0], y: d.pos[1]}))
+          .on('drag', function (d:prov.IStateAnnotation, i) {
+          const e : any = d3.event;
+          d.size = [d.pos[0] + e.x, d.pos[1] + e.y];
+          state.setAnnotation(i, d);
+          d3.select(this.parentNode).style({
+            width: (d:prov.IStateAnnotation) => d.size ? d.size[0] + 'px' : null,
+            height: (d:prov.IStateAnnotation) => d.size ? d.size[1] + 'px' : null,
+          });
+        }));
+      //rotate
+      $anns_enter.append('button').attr('tabindex', -1).attr('class', 'btn btn-default fa fa-rotate-right').call(d3.behavior.drag()
+        .origin(() => ({x : 0, y: 0}))
+        .on('drag', function (d:prov.IStateAnnotation, i) {
+          const e:any = d3.event;
+          //const base_pos = C.bounds(this);
+          //const bounds = C.bounds(this.parentNode);
+          var bak = d.rotation || 0;
+          if (e.dx > 1) {
+            d.rotation = bak + 10;
+          } else if (e.dx < -1) {
+            d.rotation = bak - 10;
+          }
+          if (d.rotation !== bak) {
+            state.setAnnotation(i, d);
+            d3.select(this.parentNode).style('transform', updateTransform);
+          }
+        }));
+    }
+    $anns.select('div').html((d) => this.renderer(d.text));
+    $anns.style({
+      left: (d:prov.IStateAnnotation) => d.pos[0] + 'px',
+      top: (d:prov.IStateAnnotation) => d.pos[1] + 'px',
+      width: (d:prov.IStateAnnotation) => d.size ? d.size[0] + 'px' : null,
+      height: (d:prov.IStateAnnotation) => d.size ? d.size[1] + 'px' : null,
+      transform: updateTransform
+    });
+
+    $anns.exit().remove();
+
+    return $anns;
+  }
+
+  renderAnnotations(state:prov.AStoryNode) {
+    return new Promise((resolve) => {
+      const $anns = this.renderAnnotationsImpl(state);
+      const editable = modeFeatures.isEditable();
+      if (editable) {
+        this.$main.append('button').attr('class', 'btn btn-default fa fa-plus-square add-text-annotation').on('click', () => {
+          state.pushAnnotation({
+            text: '',
+            pos: [100, 100]
+          });
+          this.renderAnnotationsImpl(state);
+        });
+      }
+
+
       if (this.options.animation && !$anns.empty()) {
-        $anns.transition().duration(this.options.duration).style('opacity', 1).each('end', () => {
+        $anns.style('opacity', 0).transition().duration(this.options.duration).style('opacity', 1).each('end', () => {
           resolve($anns.node());
         });
       } else {
         $anns.style('opacity', 1);
         resolve($anns.node());
       }
-      $anns.exit().remove();
     });
   }
 
   hideOld() {
     return new Promise((resolve) => {
-      const $div = this.$main.selectAll('div.text-annotation, div.text-overlay');
+      const $div = this.$main.selectAll('div.text-annotation, div.text-overlay, button.add-text-annotation');
       if (this.options.animation && !$div.empty()) {
         $div.transition().duration(this.options.duration).style('opacity', 0).each('end', () => {
           resolve();
@@ -81,34 +186,38 @@ export class Renderer {
   }
 
   renderText(overlay:prov.TextStoryNode) {
-    const renderer : any = this.options.markdown ? marked : String;
+    const that = this;
     return new Promise((resolve) => {
       var $div = this.$main.append('div').classed('text-overlay', true).attr('data-id', overlay.id).style('opacity', 0);
       var $divs = $div.selectAll('div').data([overlay.title, overlay.text]);
       $divs.enter().append('div');
-      $divs.attr('class', (d,i) => `text-overlay-${i === 0 ? 'header': 'body'}`);
+      $divs.attr('class', (d, i) => `text-overlay-${i === 0 ? 'header' : 'body'}`);
 
-      if (modeFeatures.isEditable()) {
-        let onEdit = function(d) {
-          //disable on click handler
-          const $elem = d3.select(this).on('click', null);
-          const isBody = $elem.classed('text-overlay-body');
+      const editable = modeFeatures.isEditable();
 
-          $elem.append('textarea').property('value', d).on('blur', function()  {
-            if (!isBody) {
-              overlay.title = this.value;
-            } else {
-              overlay.text = this.value;
-            }
-            //update value and enable edit click handler again
-            $elem.datum(this.value).html((d: string) => d.length === 0 ? '<i>Enter Text by Clicking (MarkDown supported)</i>' : renderer(d)).on('click', onEdit);
-          });
-        };
-        $divs.html((d: string) => d.length === 0 ? '<i>Enter Text by Clicking (MarkDown supported)</i>' : renderer(d));
-        $divs.on('click', onEdit);
-      } else {
-        $divs.html(renderer);
-      }
+      $divs.classed('editable', editable);
+      let onEdit = function (d) {
+        const $elem = d3.select(this);
+        if (!$elem.classed('editable')) {
+          return;
+        }
+        $elem.on('click', null);
+        //disable on click handler
+        const isBody = $elem.classed('text-overlay-body');
+
+        $elem.append('textarea').property('value', d).on('blur', function () {
+          if (!isBody) {
+            overlay.title = this.value;
+          } else {
+            overlay.text = this.value;
+          }
+          //update value and enable edit click handler again
+          $elem.datum(this.value).html(that.renderer).on('click', onEdit);
+        });
+      };
+      $divs.on('click', onEdit);
+
+      $divs.html(that.renderer);
 
       if (this.options.animation) {
         $div.transition().duration(this.options.duration).style('opacity', 1).each('end', () => {
@@ -120,16 +229,11 @@ export class Renderer {
       }
     });
   }
-
-  editAnnotation(anns:prov.IStateAnnotation) {
-
-  }
 }
 
 export function create(main:HTMLElement, graph:prov.ProvenanceGraph) {
   const instance = new Renderer(d3.select(main), graph);
   return {
-    render: instance.render.bind(instance),
-    edit: instance.editAnnotation.bind(instance)
+    render: instance.render.bind(instance)
   };
 }
