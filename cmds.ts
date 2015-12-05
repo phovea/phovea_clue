ca27f9254172f3725c3bc5ae1a274e75bf697afd
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-12-05 19:59:49 +0100

    transition time for clue_demo

--- cmds.ts
+++ cmds.ts
@@ -2,46 +2,63 @@
  * Created by Samuel Gratzl on 27.08.2015.
  */
 
+import C = require('../caleydo_core/main');
 import prov = require('../caleydo_provenance/main');
 import d3 = require('d3');
 
-function addElem(inputs, parameter, graph) {
-  var $main:d3.Selection<any> = inputs[0].value,
-    pos = parameter.pos,
-    desc_name = parameter.desc_name;
+function addElem(inputs, parameter, graph, within) {
+  return C.resolveIn(within).then(() => {
+    var $main:d3.Selection<any> = inputs[0].value,
+      pos = parameter.pos,
+      desc_name = parameter.desc_name;
 
 
-  var $div = $main.append('div').classed('block', true).datum(desc_name).style({
-    left: pos.x + 'px',
-    top: pos.y + 'px'
-  });
-  var $toolbar = $div.append('div').classed('toolbar', true);
-  var $body = $div.append('div').classed('body', true);
-  /*vis.list(data)[0].load().then((p) => {
-   p.factory(data, $body.node());
-   });*/
-  $body.text(desc_name);
-  var $div_ref = prov.ref($div, desc_name, prov.cat.visual);
-
-  $toolbar.append('i').attr('class', 'fa fa-close').on('click', () => {
-    graph.push(createRemoveCmd($div_ref, inputs[0]));
-  });
-  return {
-    created: [$div_ref],
-    inverse: createRemoveCmd($div_ref, inputs[0])
-  };
-}
-function removeElem(inputs, parameter, graph) {
-  var $div:d3.Selection<any> = inputs[0].value,
-    inv = createAddCmd(inputs[1], $div.datum(), {
-      x: parseInt($div.style('left'), 10),
-      y: parseInt($div.style('top'), 10)
+    var $div = $main.append('div').classed('block', true).datum(desc_name).style({
+      left: pos.x + 'px',
+      top: pos.y + 'px',
+      opacity: within > 0 ? 0 : 1
     });
-  $div.remove();
-  return {
-    removed: [inputs[0]],
-    inverse: inv
-  };
+    var $toolbar = $div.append('div').classed('toolbar', true);
+    var $body = $div.append('div').classed('body', true);
+    /*vis.list(data)[0].load().then((p) => {
+     p.factory(data, $body.node());
+     });*/
+    $body.text(desc_name);
+    var $div_ref = prov.ref($div, desc_name, prov.cat.visual);
+
+    $toolbar.append('i').attr('class', 'fa fa-close').on('click', () => {
+      graph.push(createRemoveCmd($div_ref, inputs[0]));
+    });
+    if (within > 0) {
+      $div.transition().duration(within).style('opacity', 1);
+    }
+    return {
+      created: [$div_ref],
+      inverse: createRemoveCmd($div_ref, inputs[0]),
+      consumed: within
+    };
+  });
+}
+function removeElem(inputs, parameter, graph, within) {
+  return C.resolveIn(within).then(() => {
+    var $div:d3.Selection<any> = inputs[0].value,
+      inv = createAddCmd(inputs[1], $div.datum(), {
+        x: parseInt($div.style('left'), 10),
+        y: parseInt($div.style('top'), 10)
+      });
+
+    if (within > 0) {
+      $div.transition().duration(within).style('opacity', 0).remove();
+    } else {
+      $div.remove();
+    }
+
+    return {
+      removed: [inputs[0]],
+      inverse: inv,
+      consumed: within
+    };
+  });
 }
 
 interface ID3Ref extends prov.IObjectRef<d3.Selection<any>> {
