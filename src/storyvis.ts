8dd60f1ae43b3a3aaaa13485fe22a9b6214d1de9
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-06 21:35:00 +0100

    add missing $ import

--- src/storyvis.ts
+++ src/storyvis.ts
@@ -14,6 +14,7 @@ import * as vis from 'phovea_core/src/vis';
 import * as utils from './utils';
 import * as marked from 'marked';
 import * as player from './player';
+import * as $ from 'jquery';
 
 
 interface ISlideNodeRepr {
