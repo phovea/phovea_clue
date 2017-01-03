4e3505196e04dc07dcd9736592c94b57f19b1c4a
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-03 10:49:09 +0100

    rename phovea_bootstrap_fontawesome to phovea_ui

--- src/provvis.ts
+++ src/provvis.ts
@@ -9,7 +9,7 @@ import * as ranges from 'phovea_core/src/range';
 import * as idtypes from 'phovea_core/src/idtype';
 import * as provenance from 'phovea_core/src/provenance';
 import * as cmode from './mode';
-import * as dialogs from 'phovea_bootstrap_fontawesome/src/dialogs';
+import * as dialogs from 'phovea_ui/src/dialogs';
 import * as d3 from 'd3';
 import * as vis from 'phovea_core/src/vis';
 
