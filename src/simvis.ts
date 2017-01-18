fbdd07fa9c2c6fb37d193a3760dee1e54cfa6ec6
Holger Stitz <holger.stitz@jku.at>
2017-01-18 15:35:52 +0100

    Further refactoring of SimilarityHash

--- src/simvis.ts
+++ src/simvis.ts
@@ -8,7 +8,7 @@ import * as idtypes from 'phovea_core/src/idtype';
 import * as provenance from 'phovea_core/src/provenance';
 import * as vis from 'phovea_core/src/vis';
 
-import {SimHash} from 'phovea_core/src/provenance/SimilarityHash';
+import {SimHash, ISimilarityCategory} from 'phovea_core/src/provenance/SimilarityHash';
 import {mod} from 'phovea_core/src/index';
 import StateNode from 'phovea_core/src/provenance/StateNode';
 import ProvenanceGraph from 'phovea_core/src/provenance/ProvenanceGraph';
@@ -16,11 +16,12 @@ import {SimVisStateNode} from 'phovea_core/src/provenance/StateNode';
 import {MatchedTokenTree} from 'phovea_core/src/provenance/token/MatchedTokenTree';
 
 
-interface IWeight {
-  name;
-  value;
-  color;
-  active;
+interface ILineUpColumnDesc {
+  label: string;
+  type: string;
+  column: string;
+  domain: number[];
+  color: string;
 }
 
 
@@ -53,7 +54,7 @@ export class LineupStateView extends vis.AVisInstance {
     if (this.executionToStateRunning === null) {
       this.executionToStateRunning = state;
     } else {
-      throw Error('Two executions are run in paralell');
+      throw Error('Two executions are run in parallel');
     }
   }
 
@@ -111,7 +112,7 @@ export class LineupStateView extends vis.AVisInstance {
 
   updateWeights() {
     let width:number = 48;
-    let weights = SimHash.hasher.categoryWeighting;
+    let weights = SimHash.getWeighting();
     this.lstack.setWeights(weights);
     this.lstack.setWidth(width);
     this.cstack.setWeights(weights);
@@ -123,52 +124,36 @@ export class LineupStateView extends vis.AVisInstance {
 
 
   initialize(reason:SimVisStateNode = null) {
-    const desc = [
-      {label: 'data', type: 'number', column: 'ld', 'domain': [0, 1], color: SimHash.COLORS[0]},
-      {label: 'visual', type: 'number', column: 'lv', 'domain': [0, 1], color: SimHash.COLORS[1]},
-      {label: 'selection', type: 'number', column: 'ls', 'domain': [0, 1], color: SimHash.COLORS[2]},
-      {label: 'layout', type: 'number', column: 'll', 'domain': [0, 1], color: SimHash.COLORS[3]},
-      {label: 'analysis', type: 'number', column: 'la', 'domain': [0, 1], color: SimHash.COLORS[4]},
-      {label: 'data', type: 'number', column: 'cd', 'domain': [0, 1], color: SimHash.COLORS[0]},
-      {label: 'visual', type: 'number', column: 'cv', 'domain': [0, 1], color: SimHash.COLORS[1]},
-      {label: 'selection', type: 'number', column: 'cs', 'domain': [0, 1], color: SimHash.COLORS[2]},
-      {label: 'layout', type: 'number', column: 'cl', 'domain': [0, 1], color: SimHash.COLORS[3]},
-      {label: 'analysis', type: 'number', column: 'ca', 'domain': [0, 1], color: SimHash.COLORS[4]},
-      {label: 'data', type: 'number', column: 'rd', 'domain': [0, 1], color: SimHash.COLORS[0]},
-      {label: 'visual', type: 'number', column: 'rv', 'domain': [0, 1], color: SimHash.COLORS[1]},
-      {label: 'selection', type: 'number', column: 'rs', 'domain': [0, 1], color: SimHash.COLORS[2]},
-      {label: 'layout', type: 'number', column: 'rl', 'domain': [0, 1], color: SimHash.COLORS[3]},
-      {label: 'analysis', type: 'number', column: 'ra', 'domain': [0, 1], color: SimHash.COLORS[4]}
-    ];
-    //this.arr = [{'ld':1, 'lv':1, 'ls':1, 'll':1, 'la':1,'cd':1, 'cv':1, 'cs':1, 'cl':1, 'ca':1,'rd':1, 'rv':1, 'rs':1, 'rl':1, 'ra':1}]
+    const createDesc = (pos:string):ILineUpColumnDesc[] => {
+      return SimHash.CATEGORIES2.map((d) => {
+        return {
+          label: d.name,
+          type: 'number',
+          column: pos + d.name[0],
+          domain: [0,1],
+          color: d.color
+        };
+      });
+    };
+
+    const descLeft = createDesc('l');
+    const descCenter = createDesc('c');
+    const descRight = createDesc('r');
 
     this.fillArr(reason);
-    this.luDataProvider = new lineup.provider.LocalDataProvider(this.arr, desc);
+    this.luDataProvider = new lineup.provider.LocalDataProvider(this.arr, [...descLeft, ...descCenter, ...descRight]);
     const r = this.luDataProvider.pushRanking();
 
-    this.lstack = this.luDataProvider.create(lineup.model.createStackDesc('Active'));
-    r.push(this.lstack);
-    this.lstack.push(this.luDataProvider.create(desc[0]));
-    this.lstack.push(this.luDataProvider.create(desc[1]));
-    this.lstack.push(this.luDataProvider.create(desc[2]));
-    this.lstack.push(this.luDataProvider.create(desc[3]));
-    this.lstack.push(this.luDataProvider.create(desc[4]));
+    const createStackedColumn = (label:string, desc:ILineUpColumnDesc[]) => {
+      const stackedColumn = this.luDataProvider.create(lineup.model.createStackDesc(label));
+      r.push(stackedColumn);
+      desc.forEach((d) => stackedColumn.push(this.luDataProvider.create(d)));
+      return stackedColumn;
+    };
 
-    this.cstack = this.luDataProvider.create(lineup.model.createStackDesc('Intersection'));
-    r.push(this.cstack);
-    this.cstack.push(this.luDataProvider.create(desc[5]));
-    this.cstack.push(this.luDataProvider.create(desc[6]));
-    this.cstack.push(this.luDataProvider.create(desc[7]));
-    this.cstack.push(this.luDataProvider.create(desc[8]));
-    this.cstack.push(this.luDataProvider.create(desc[9]));
-
-    this.rstack = this.luDataProvider.create(lineup.model.createStackDesc('Ranked'));
-    r.push(this.rstack);
-    this.rstack.push(this.luDataProvider.create(desc[10]));
-    this.rstack.push(this.luDataProvider.create(desc[11]));
-    this.rstack.push(this.luDataProvider.create(desc[12]));
-    this.rstack.push(this.luDataProvider.create(desc[13]));
-    this.rstack.push(this.luDataProvider.create(desc[14]));
+    this.lstack = createStackedColumn('Active', descLeft);
+    this.cstack = createStackedColumn('Intersection', descCenter);
+    this.rstack = createStackedColumn('Ranked', descRight);
 
     this.lu = lineup.create(this.luDataProvider, this.node, {
       /*
@@ -234,14 +219,11 @@ export class LineupStateView extends vis.AVisInstance {
 
 export class WeightInterface {
 
-  protected cats = SimHash.CATEGORIES;
-  protected weights:IWeight[] = [];
   protected cumSum:number[] = [];
   protected scalefactor:number = (300 - 4) / 100;
 
   protected catContainer;
   protected barContainer;
-  protected faString:string[] = ['fa-database', 'fa-bar-chart', 'fa-pencil-square', 'fa-desktop', 'fa-gear'];
 
   protected closeWeightSelection;
   protected openWeightSelection;
@@ -249,17 +231,11 @@ export class WeightInterface {
   constructor(container) {
     this.catContainer = container;
     this.barContainer = this.catContainer.select('.barContainer');
-    let rawWeights = SimHash.hasher.categoryWeighting;
+    let rawWeights = SimHash.getWeighting();
     this.cumSum[0] = 0;
     for (let i = 1; i <= rawWeights.length; i++) {
       this.cumSum[i] = this.cumSum[i - 1] + rawWeights[i - 1];
     }
-    const cols = SimHash.COLORS;
-    this.weights[0] = {name: this.cats[0], value: rawWeights[0], color: cols[0], active: true};
-    this.weights[1] = {name: this.cats[1], value: rawWeights[1], color: cols[1], active: true};
-    this.weights[2] = {name: this.cats[2], value: rawWeights[2], color: cols[2], active: true};
-    this.weights[3] = {name: this.cats[3], value: rawWeights[3], color: cols[3], active: true};
-    this.weights[4] = {name: this.cats[4], value: rawWeights[4], color: cols[4], active: true};
 
     //this.update(false);
     this.initialize();
@@ -270,14 +246,14 @@ export class WeightInterface {
     this.closeWeightSelection();
   }
 
-  protected catsWeightMap(name):IWeight {
-    return this.weights[this.cats.indexOf(name)];
+  protected catsWeightMap(name):ISimilarityCategory {
+    return SimHash.CATEGORIES2.filter((d) => d.name === name)[0];
   }
 
   protected getNextActive(index) {
     let nextIndex = -1;
-    for (let i = 1; i < this.weights.length; i++) {
-      if (this.weights[mod(index + i, 5)].active) {
+    for (let i = 1; i < SimHash.CATEGORIES2.length; i++) {
+      if (SimHash.CATEGORIES2[mod(index + i, 5)].active) {
         nextIndex = mod(index + i, 5);
         break;
       }
@@ -287,8 +263,8 @@ export class WeightInterface {
 
   protected getPreviousActive(index) {
     let nextIndex = -1;
-    for (let i = 1; i < this.weights.length; i++) {
-      if (this.weights[mod(index - i, 5)].active) {
+    for (let i = 1; i < SimHash.CATEGORIES2.length; i++) {
+      if (SimHash.CATEGORIES2[mod(index - i, 5)].active) {
         nextIndex = mod(index - i, 5);
         break;
       }
@@ -301,11 +277,11 @@ export class WeightInterface {
     let _that = this;
     let transitionDuration = 300;
     let bars = this.barContainer.selectAll('div')
-      .data(this.weights, function (d) {
+      .data(SimHash.CATEGORIES2, function (d) {
         return d.name;
       });
     let lines = d3.select('.lineContainer').selectAll('line')
-      .data(this.weights, function (d) {
+      .data(SimHash.CATEGORIES2, function (d) {
         return d.name;
       });
 
@@ -330,7 +306,7 @@ export class WeightInterface {
     }
     b.style('left', '0px')
       .style('height', function (d) {
-        return d.value * _that.scalefactor + 'px';
+        return d.weight * _that.scalefactor + 'px';
       })
       .style('top', function (d, i) {
         return _that.cumSum[i] * _that.scalefactor + 'px';
@@ -365,16 +341,6 @@ export class WeightInterface {
         return d3.hsl(_that.catsWeightMap($(this).attr('title')).color).brighter(0.7).toString();
       });
 
-
-    //set weights
-    let w = [0, 0, 0, 0, 0];
-    w[_that.cats.indexOf(_that.weights[0].name)] = _that.weights[0].value;
-    w[_that.cats.indexOf(_that.weights[1].name)] = _that.weights[1].value;
-    w[_that.cats.indexOf(_that.weights[2].name)] = _that.weights[2].value;
-    w[_that.cats.indexOf(_that.weights[3].name)] = _that.weights[3].value;
-    w[_that.cats.indexOf(_that.weights[4].name)] = _that.weights[4].value;
-    SimHash.hasher.categoryWeighting = w;
-
     //update handlePos
     let handles = this.catContainer.selectAll('.chart_handle');
     let h = <any>handles;
@@ -387,7 +353,7 @@ export class WeightInterface {
       })
       .style('opacity', function () {
         let setActive = _that.catsWeightMap($(this).attr('id')).active;
-        let index = _that.cats.indexOf($(this).attr('id'));
+        let index = SimHash.CATEGORIES2.findIndex((d) => d.name === $(this).attr('id'));
         if (_that.getNextActive(index) <= index) {
           setActive = false;
         }
@@ -403,7 +369,7 @@ export class WeightInterface {
       label = label.transition().duration(transitionDuration);
     }
     label.attr('value', function () {
-      return Math.round(_that.catsWeightMap($(this).attr('id')).value) / 100;
+      return Math.round(SimHash.CATEGORIES2.filter((d) => d.name === $(this).attr('id'))[0].weight) / 100;
     });
   }
 
@@ -424,11 +390,9 @@ export class WeightInterface {
     };
 
     const $controlContainer = $('.controlContainer');
-    $controlContainer.append(categoryUnit(this.weights[0].name, this.weights[0].value, this.faString[0]));
-    $controlContainer.append(categoryUnit(this.weights[1].name, this.weights[1].value, this.faString[1]));
-    $controlContainer.append(categoryUnit(this.weights[2].name, this.weights[2].value, this.faString[2]));
-    $controlContainer.append(categoryUnit(this.weights[3].name, this.weights[3].value, this.faString[3]));
-    $controlContainer.append(categoryUnit(this.weights[4].name, this.weights[4].value, this.faString[4]));
+    SimHash.CATEGORIES2.forEach((d) => {
+      $controlContainer.append(categoryUnit(d.name, d.weight, d.icon));
+    });
     $('.provenance-similarity-vis').hide();
 
     $('.categoryUnit label input[type=checkbox]').prop('checked', true);
@@ -441,10 +405,9 @@ export class WeightInterface {
     };
 
     const $catWeightContainer = $('.catWeightContainer');
-    $catWeightContainer.append(handleHtml(this.cats[0]));
-    $catWeightContainer.append(handleHtml(this.cats[1]));
-    $catWeightContainer.append(handleHtml(this.cats[2]));
-    $catWeightContainer.append(handleHtml(this.cats[3]));
+    SimHash.CATEGORIES2.forEach((d) => {
+      $catWeightContainer.append(handleHtml(d.name));
+    });
 
     const dragResize = d3.behavior.drag()
       .on('drag', function () {
@@ -452,21 +415,21 @@ export class WeightInterface {
         if (x > 100) {
           x = 100;
         }
-        let id = _that.cats.indexOf($(this).attr('id'));
+        let id = SimHash.CATEGORIES2.findIndex((d) => d.name === $(this).attr('id'));
         let diff = _that.cumSum[id + 1] - x;
-        _that.weights[id].value -= diff;
+        SimHash.CATEGORIES2[id].weight -= diff;
         let next = _that.getNextActive(id);
         let prev = _that.getPreviousActive(id);
         //let isLast = next <= id;
         if (next <= id) {
-          _that.weights[prev].value += diff;
+          SimHash.CATEGORIES2[prev].weight += diff;
         } else {
-          _that.weights[next].value += diff;
+          SimHash.CATEGORIES2[next].weight += diff;
         }
 
         _that.cumSum[0] = 0;
-        for (let i = 1; i <= _that.weights.length; i++) {
-          _that.cumSum[i] = _that.cumSum[i - 1] + _that.weights[i - 1].value;
+        for (let i = 1; i <= SimHash.CATEGORIES2.length; i++) {
+          _that.cumSum[i] = _that.cumSum[i - 1] + SimHash.CATEGORIES2[i - 1].weight;
         }
         _that.update(false);
         SimHash.hasher.fire('weights_changed');
@@ -515,15 +478,15 @@ export class WeightInterface {
         .duration(100)
         .style('height', '22px');
       this.barContainer.selectAll('.adjustable').transition()
-        .text(function (d, i) {
-          return (_that.cats[i] + ' ' + Math.round(d.value) + '%');
+        .text(function (d) {
+          return (d.name + ' ' + Math.round(d.weight) + '%');
         })
         .style('top', '0px')
         .style('left', function (d, i) {
           return _that.cumSum[i] * _that.scalefactor + 'px';
         })
         .style('width', function (d) {
-          return d.value * _that.scalefactor + 'px';
+          return d.weight * _that.scalefactor + 'px';
         })
         .style('height', '22px')
         .style('background-color', function (d) {
@@ -566,7 +529,7 @@ export class WeightInterface {
       this.barContainer.selectAll('.compact').transition()
         .style('left', '0px')
         .style('height', function (d) {
-          return d.value * _that.scalefactor + 'px';
+          return d.weight * _that.scalefactor + 'px';
         })
         .style('top', function (d, i) {
           return _that.cumSum[i] * _that.scalefactor + 'px';
@@ -588,26 +551,26 @@ export class WeightInterface {
     this.catContainer.on('mouseleave', this.closeWeightSelection);
 
     d3.selectAll('.categoryUnit label input').on('change', function () {
-      let index = _that.cats.indexOf($(this).attr('value'));
-      if (_that.weights[index].active) {
+      let index = SimHash.CATEGORIES2.findIndex((d) => d.name === $(this).attr('value'));
+      if (SimHash.CATEGORIES2[index].active) {
         //deactivate
-        _that.weights[_that.getNextActive(index)].value += _that.weights[index].value;
-        _that.weights[index].value = 0;
+        SimHash.CATEGORIES2[_that.getNextActive(index)].weight += SimHash.CATEGORIES2[index].weight;
+        SimHash.CATEGORIES2[index].weight = 0;
       } else {
         //activate
         let nextIndex = _that.getNextActive(index);
         if (nextIndex < 0) {
-          _that.weights[index].value = 100;
+          SimHash.CATEGORIES2[index].weight = 100;
         } else {
-          let val = _that.weights[nextIndex].value;
-          _that.weights[index].value = val / 2;
-          _that.weights[nextIndex].value = val / 2;
+          let val = SimHash.CATEGORIES2[nextIndex].weight;
+          SimHash.CATEGORIES2[index].weight = val / 2;
+          SimHash.CATEGORIES2[nextIndex].weight = val / 2;
         }
       }
-      _that.weights[index].active = !_that.weights[index].active;
+      SimHash.CATEGORIES2[index].active = !SimHash.CATEGORIES2[index].active;
       _that.cumSum[0] = 0;
-      for (let i = 1; i <= _that.weights.length; i++) {
-        _that.cumSum[i] = _that.cumSum[i - 1] + _that.weights[i - 1].value;
+      for (let i = 1; i <= SimHash.CATEGORIES2.length; i++) {
+        _that.cumSum[i] = _that.cumSum[i - 1] + SimHash.CATEGORIES2[i - 1].weight;
       }
       _that.update(true);
       SimHash.hasher.fire('weights_changed');
@@ -873,10 +836,7 @@ export class TokenTreeVisualization {
           return `<div class="nonPairedToken">`;
         }
         let bgcolor:string = d.isLeafNodeWithoutDummyChilds ? SimHash.getCategoryColor(d.categoryName) : 'white';
-        let html = '';
-        let text = d.name;
-        html = `<div title="${text}" class="token center" style="background-color: ${bgcolor}">${text}</div>`;
-        return html;
+        return `<div title="${d.name}" class="token center" style="background-color: ${bgcolor}">${d.name}</div>`;
       });
 
     nodeEnter.selectAll('.visStateDescription')
