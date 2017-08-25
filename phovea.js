3dda3dda6abcb5dc9f742b05638ae74e70b3255b
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-08-25 08:08:35 +0200

    remove d3 dependency and use feature flags

--- phovea.js
+++ phovea.js
@@ -8,24 +8,27 @@
 module.exports = function(registry) {
   //registry.push('extension-type', 'extension-id', function() { return System.import('./src/extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push('actionFunction', 'select', function() { return System.import('./src/selection'); }, {
+  /// #if include('clue', 'selection')
+  registry.push('actionFunction', 'select', function() { return import('./src/selection'); }, {
     'factory': 'select'
   });
 
-  registry.push('actionCompressor', 'idtype-selection', function() { return System.import('./src/selection'); }, {
+  registry.push('actionCompressor', 'idtype-selection', function() { return import('./src/selection'); }, {
     'factory': 'compressSelection',
     'matches': 'select'
   });
+  /// #endif
 
-  registry.push('actionFunction', 'transform', function() { return System.import('./src/multiform'); }, {
+  /// #if include('clue', 'multiform')
+  registry.push('actionFunction', 'transform', function() { return import('./src/multiform'); }, {
     'factory': 'transform'
   });
-  registry.push('actionFunction', 'changeVis', function() { return System.import('./src/multiform'); }, {
+  registry.push('actionFunction', 'changeVis', function() { return import('./src/multiform'); }, {
     'factory': 'changeVis'
   });
-  registry.push('actionFunction', 'select', function() { return System.import('./src/multiform'); }, {
+  registry.push('actionFunction', 'select', function() { return import('./src/multiform'); }, {
     'factory': 'select'
   });
+  /// #endif
   // generator-phovea:end
 };
-
