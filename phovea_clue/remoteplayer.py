5f93ec788dd085cba976da23b8f56928c4c37055
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2019-09-12 14:34:03 +0200

    use `string.ascii_lowercase`
    instead of `string.lowercase`

--- phovea_clue/remoteplayer.py
+++ phovea_clue/remoteplayer.py
@@ -74,7 +74,7 @@ screenshotter = Screenshotter()
 def randomword(length):
   import random
   import string
-  return ''.join(random.choice(string.lowercase) for i in range(length))
+  return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 
 def generate_url(app, prov_id, state):
