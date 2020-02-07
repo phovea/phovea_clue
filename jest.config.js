d1f0fae5f936ba55f9a55693ac94db34a1fc403a
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-07 10:24:00 +0100

    fix jest config
    remove node modules from testing and use correct
     plugins for `pluginsToTransform`

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
 // test dependencies that require transformation
 const pluginsToTransform = [
-  'tdp_comments',
-  'tdp_ui',
-  'tdp_core',
   'bootstrap-sass', // required to transform for phovea_ui
   'phovea_ui',
-  'phovea_clue',
   'phovea_core',
-  'phovea_security_flask',
-  'sandbox',
-  'tdp_marvinjs'
+  'phovea_security_flask'
 ].join('|');
 
 /**
@@ -33,7 +27,6 @@ module.exports = {
   ],
   modulePaths: [
     "src",
-    "../node_modules",
     "../"
   ],
   transformIgnorePatterns: [`../node_modules/(?!${pluginsToTransform})`],
