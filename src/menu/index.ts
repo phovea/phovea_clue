d857350568380cb52cfc4294dab51404e1d3e77d
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 12:07:02 +0200

    add src/menu/index.ts

---
+++ src/menu/index.ts
@@ -0,0 +1,4 @@
+export * from './ProvenanceGraphMenu';
+export {default as ProvenanceGraphMenu} from './ProvenanceGraphMenu';
+export * from './LoginMenu';
+export {default as LoginMenu} from './LoginMenu';
