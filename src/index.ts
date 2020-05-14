8b7a65d1f88bc869024f1d6f21f80babf7341f97
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 12:09:41 +0200

    add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './menu';
+export * from './vis';
+export * from './wrapper';
+
+export * from './annotation';
+export * from './CLUEGraphManager';
+export * from './compress';
+export * from './EmbeddedCLUE';
+export * from './extensions';
+export * from './mode';
+export * from './multiform';
+export {default} from './phovea';
+export * from './player';
+export * from './selection';
+export * from './utils';
